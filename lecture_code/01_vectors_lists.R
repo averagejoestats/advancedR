8fc8c99c078fd371a74272be249a2b441dbae6b2
Average Joe <averagejoestats@gmail.com>
2024-01-25 16:08:43 -0500

    code added during lecture

--- lecture_code/01_vectors_lists.R
+++ lecture_code/01_vectors_lists.R
@@ -13,12 +13,67 @@
 
 # R function documentation
 # - tip: don't guess wildly
+?seq
+typeof( seq( 1, 10 ) )
+typeof( 1:10 )
+typeof( c(1,2,3,4,5,6,7,8,9,10) )
+
+seq_len( 7 )
+seq_along( rnorm(10) )
+seq_along( numeric(0) )
+
+a <- rnorm(10)
+1:length(a)
+
+a <- numeric(0)
+1:length(a)
+
 
 # atomic vector automatic coercion
 # - accidental coercion
 # - intentional coercion (sum use case)
 
+a <- c(1.3, 6.8, 4.3 )
+b <- c( a, 1L )
+typeof(b[1])
+typeof(b[4])
+typeof(b)
+
+d <- c( b, "9.2" )
+d
+
+b
+as.character(b)
+b
+b <- as.character(b)
+b
+
+a
+typeof( sum( a > 4 ) )
+typeof( sum( as.integer( a > 4 ) ) )
+
 # matrix examples
 
 
+
 # list examples
+list()
+c()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

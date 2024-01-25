e4e13a4796c55f4edae115a3f1588bfa751f7233
Average Joe <averagejoestats@gmail.com>
2024-01-25 15:16:37 -0500

    first draft of lecture code

---
+++ lecture_code/01_vectors_lists.R
@@ -0,0 +1,24 @@
+
+# look at the course repo.
+
+# make a new directory for your personal version of the files
+
+# how to pull new files
+
+# what to do if you have a conflict
+
+
+# Course tip: attempt at least 1 part of 1 exercise before tuesday class
+
+
+# R function documentation
+# - tip: don't guess wildly
+
+# atomic vector automatic coercion
+# - accidental coercion
+# - intentional coercion (sum use case)
+
+# matrix examples
+
+
+# list examples

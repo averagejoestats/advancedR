f32142ecc6183b3396b964edf0d47e89064fa1a3
Average Joe <averagejoestats@gmail.com>
2024-02-29 15:14:57 -0500

    ithaca weather plotting

---
+++ lecture_code/plot_ithaca_weather.R
@@ -0,0 +1,65 @@
+
+#data_dir <- "/home/joe/Dropbox/teaching/repos/advancedR/datasets"
+
+# download most recent file from web:
+#fname <- "CRNH0203-2023-NY_Ithaca_13_E.txt"
+#url <- paste0("https://www.ncei.noaa.gov/pub/data/uscrn/products/hourly02/2023/",fname)
+#download.file(url, file.path(data_dir, fname) )
+
+# search for the weather datasets
+fnames <- list.files( "../datasets", pattern = "Ithaca" )
+fnames
+
+# read in the first dataset to initialize a data frame
+dat <- read.table( file.path( data_dir, fnames[1] ), header = FALSE )
+
+# loop over the rest of the files and rbind them
+for(j in 2:length(fnames)){
+    dat <- rbind(
+        dat,
+        read.table( file.path( data_dir, fnames[j] ), header = FALSE )
+    )
+}
+
+# Create some data variables for plotting purposes
+dat$date <- as.Date( as.character(dat$V2), format = "%Y%m%d" )
+dat$year <- as.numeric( format(dat$date, "%Y"))
+dat$doy  <- as.numeric( format(dat$date, "%j"))
+dat$dom  <- as.numeric( format(dat$date, "%d"))
+dat$dom_decimal <- dat$dom + dat$V3/2400
+dat$doy_decimal <- dat$doy + dat$V3/2400
+dat$month <- as.numeric( format(dat$date, "%m" ) )
+dat$temp <- dat$V9
+dat$temp[ dat$temp < -1000 ] <- NA
+dat$tempF <- dat$temp*9/5 + 32
+
+
+# plot the february data, highlighting one year
+plot(
+    NA, type = "n",
+    xlim = c(1,29), ylim = c(-40,80),
+    xlab = "February", ylab = "Temperature"
+)
+for( y in 2004:2023 ){
+    # grab data for february
+    ii <- dat$year == y & dat$month == 2
+    # plot the data
+    lines( dat$dom_decimal[ii], dat$tempF[ii], col = "gray" )
+    if( y == 2023 ){
+        lines( dat$dom_decimal[ii], dat$tempF[ii], lwd = 2, col = "black" )
+    }
+}
+mtext("February Temperatures Ithaca 2004-2023",line = 1 )
+abline( 32, 0, col = "blue", lty = 2 )
+
+
+# some exercises:
+
+# 1. Write a function that takes in a vector of years and colors, and 
+#    highlights those years with those colors
+# 2. Add a legend for the years (see ?legend)
+# 3. Calculate the mean or median temperature for each hour, and add a line
+#    for the median.
+# 4. Calculate the median over all years over a 24 hour moving window, and
+#    add a line for that median.
+
